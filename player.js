3ef8e9376901fc3475af55c51ff219a233fb5ef5
Noah-Jaffe <noah0m0jaffe@gmail.com>
2023-08-23 13:26:29 -0400

    enable all func to be static

--- player.js
+++ player.js
@@ -5,7 +5,7 @@ class Util {
    * @param max_attempts <int> the number of times to attempt before aborting.
    * @throws Exceptions that are raised while fetching the url (not including NetworkErrors), or an offline timeout error.
    */
-  async get_successful_fetch(url, max_attempts = 10) {
+  static async get_successful_fetch(url, max_attempts = 10) {
     function delay_until_connection(timeout_sec) {
       // Sleep until onLine or timeout.
       let end_at = Date.now() + timeout_sec * 1000;
@@ -37,7 +37,7 @@ class Util {
    * @param url <String> The url to extract JSON from.
    * @returns result of JSON.parse.
    */
-  async get_JSON(url) {
+  static async get_JSON(url) {
     Logger.log(`get_JSON(${url})`,10);
     let response = await get_successful_fetch(url);
     let data = await response.json();
@@ -48,11 +48,11 @@ class Util {
    * @param sec <number> seconds.
    * @returns <String> simplified timestamp.
    */
-  sec_to_human_readable_timestamp(sec) {return `${String(parseInt(sec / 3600)).padStart(2, "0")}:${String(parseInt((sec % 3600) / 60)).padStart(2, "0")}:${parseInt(sec) % 60}`;}
+  static sec_to_human_readable_timestamp(sec) {return `${String(parseInt(sec / 3600)).padStart(2, "0")}:${String(parseInt((sec % 3600) / 60)).padStart(2, "0")}:${parseInt(sec) % 60}`;}
   /**
    * @returns true if on mobile device, false otherwise.
    */
-  mobileCheck() {
+  static mobileCheck() {
     return (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(navigator.userAgent || navigator.vendor || window.opera) ||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test((navigator.userAgent || navigator?.vendor || window.opera).substr(0, 4)));
   }
 }
@@ -63,7 +63,7 @@ class Logger {
    * @param {*} val the value to write
    * @param {Number} timeout the seconds before the log message is destroyed, <=0 for no distruction.
    */
-  log(val, timeout = 0) {
+  static log(val, timeout = 0) {
     console.log(val);
     var ts = document.createTextNode(
       new Date().toLocaleTimeString("en-us", {
@@ -98,7 +98,7 @@ class Logger {
    * @param {HTMLElement} el the element to be dissapered
    * @param {*} seconds the seconds to dissapear after
    */
-  async removeFadeOutAfter(el, seconds) {
+  static async removeFadeOutAfter(el, seconds) {
     setTimeout(() => {
       el.style.transition = "opacity 1s ease";
       el.style.opacity = 0;
@@ -109,11 +109,12 @@ class Logger {
   }
 }
 
+/*
 class Mixcloud {
   /**
    * @param key <String> the mixcloud key/url endpoint of the song to be retrieved.
    * @returns <String> the url for the audio source of the given mixcloud key.
-   */
+   * /
   static async retreive_audio_src(key) {
     write2log(`retreive_audio_src(${key})`);
     // Attempts to get the url for the src of the given mixcloud key
@@ -132,8 +133,8 @@ class Mixcloud {
    * When successful it will update the src of the live PLAYLIST_SONG record.
    * This is a background thread!
    * @param src <str> the audio source string (url) to be checked for updates.
-   */
-  check_for_updated_src(src) {
+   * /
+  static check_for_updated_src(src) {
     if (!src) {
       return null;
     }
@@ -157,7 +158,7 @@ class Mixcloud {
   }
 }
 
-/*class Player {
+class Player {
     constructor(serialized=null) {
         this.playbackOrder = serialized?.playbackOrder ? serialized.playbackOrder : 'shuffle';
         this.volume = serialized?.volume ? serialized.volume : 1;
